f2659451355839a82ffb4ab8d6beb9396e4fe5f9
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-06 19:43:14 -0300

    Modificaciones de LINQ

--- Linq1/Program.cs
+++ Linq1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//¿Qué es LINQ?
+//Language Integrated Query
+
+// 1. Nuesto origen de datos
+int[] numbers = new int[10] { 1,2,3,4,5,6,7,8,9,10};
+
+// 2. Obtener datos con una consulta
+
+/// Ejemplo: seleccionar los numero pares
+
+ var impares =  
+    from numero in numbers
+    where ( numero % 2 ) != 0
+    select numero;
+
+ var consulta =
+    from numero in numbers
+    where (numero % 2) == 0
+    select numero;
+
+// 3. Ejecutar la consulta
+foreach (var num in consulta) {
+    Console.Write(num + " ");
+}
+
+Console.WriteLine("\n Numeros impares:");
+foreach (var num in impares)
+{
+    Console.Write(num + " ");
+}
+
+//Utilizar en nuestro programa
